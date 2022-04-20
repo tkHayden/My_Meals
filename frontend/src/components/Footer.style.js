9833f356ff7cb66dc134ef1d1c5514f09dda880c
tkhayden <tylerkh805@gmail.com>
2022-04-19 18:29:12 -0700

    Footer styling

---
+++ frontend/src/components/Footer.style.js
@@ -0,0 +1,7 @@
+import {styled} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+export const LinkBox = styled(Box)(({theme}) => ({
+  display: 'flex',
+  justifyContent: 'center',
+}));
