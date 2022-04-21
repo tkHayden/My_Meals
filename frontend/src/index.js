37f5a7f15638a9958176940956800961a6bdc2e3
tkhayden <tylerkh805@gmail.com>
2022-04-20 19:14:07 -0700

    oauth context provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {Auth0Provider} from '@auth0/auth0-react';
+
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+console.log(domain);
 
 ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <Auth0Provider
+        domain= {domain}
+        clientId= {clientId}
+        redirectUri={window.location.origin}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
 
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-
-      </BrowserRouter>
+        </BrowserRouter>
+      </Auth0Provider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
