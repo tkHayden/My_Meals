14bb18c778b64db16abc8d47bed76c815b85b85c
tkhayden <tylerkh805@gmail.com>
2022-04-29 13:07:03 -0700

    refactored styling to own file for RecipeList

--- frontend/src/components/RecipeView/RecipeList.js
+++ frontend/src/components/RecipeView/RecipeList.js
@@ -1,41 +1,31 @@
 import React from 'react';
-import {Box, Grid, Typography, Card, CardActionArea,
+import {recipeCard, recipeTitle, recipeContainer} from './RecipeList.style';
+import {Box, Grid, Typography, Card,
   CardContent, CardMedia, Divider} from '@mui/material';
-import {keyframes} from '@mui/system';
 
 const RecipeList = (props) => {
-  const spin = keyframes`
-  from {
-    transform: translateY(2%);
-    opacity: 0;
-  }
-  to {
-    transform: translateX(0%);
-    opacity: 2
-  }
-`;
   const featuredList = () => {
     console.log(props);
     return (
       <>
-        {props.recipes.map((i) => {
+        {props.recipes.map((recipe) => {
           return (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={i.title}>
+            <Grid item xs={12} sm={6} md={4} lg={3} key={recipe.title}>
               <Card
-                sx={{'maxWidth': '90%',
-                  'backgroundColor': '#FAFAFA',
-                  'height': {xs: 280, sm: 290, md: 300, lg: 310}, 'm': 2, ':hover': {boxShadow: 20}}}>
+                sx={{...recipeCard}}>
                 <CardMedia
                   component="img"
                   height='200'
-                  image= {i.image}
-                  alt="green iguana"
+                  image= {recipe.image}
+                  alt= {`picture of ${recipe.title}`}
                 />
                 <Divider/>
                 <CardContent >
                   <Typography variant="h6" component="div"
-                    sx={{textAlign: 'center', fontSize: {xs: 16, sm: 17, md: 18, lg: 20}}}>
-                    {i.title.length > 45 ? `${i.title.substring(0, 45)}...` : i.title }
+                    sx={{...recipeTitle}}>
+                    {recipe.title.length > 45 ?
+                     `${recipe.title.substring(0, 45)}...` :
+                      recipe.title }
                   </Typography>
 
                 </CardContent>
@@ -48,7 +38,7 @@ const RecipeList = (props) => {
   };
 
   return (
-    <Box sx={{display: 'flex', justifyContent: 'center', animation: `${spin} 2s ease`}}>
+    <Box sx={{...recipeContainer}}>
       <Grid container spacing={1}
         sx={{width: {xs: '100%', md: '90%', xl: 1500}}}>
         <Grid item xs={12} sx={{mb: 3}}>
---
+++ frontend/src/components/RecipeView/RecipeList.style.js
@@ -0,0 +1,43 @@
+import {keyframes} from '@mui/system';
+
+const fadeUp = keyframes`
+from {
+  transform: translateY(2%);
+  opacity: 0;
+}
+to {
+  transform: translateX(0%);
+  opacity: 2
+}
+`;
+
+export const recipeCard = {
+  'maxWidth': '90%',
+  'backgroundColor': '#FAFAFA',
+  'height': {
+    xs: 280,
+    sm: 290,
+    md: 300,
+    lg: 310,
+  },
+  'm': 2,
+  ':hover': {
+    boxShadow: 20,
+  },
+};
+
+export const recipeTitle = {
+  textAlign: 'center',
+  fontSize: {
+    xs: 16,
+    sm: 17,
+    md: 18,
+    lg: 20,
+  },
+};
+
+export const recipeContainer = {
+  display: 'flex',
+  justifyContent: 'center',
+  animation: `${fadeUp} 2s ease`,
+};
