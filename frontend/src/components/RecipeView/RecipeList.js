0e2c2f93dcb53e9d2cd24c7763ce6341a0f38c2e
tkhayden <tylerkh805@gmail.com>
2022-04-29 18:21:58 -0700

    added condition when results are empty

--- frontend/src/components/RecipeView/RecipeList.js
+++ frontend/src/components/RecipeView/RecipeList.js
@@ -2,39 +2,49 @@ import React from 'react';
 import {recipeCard, recipeTitle, recipeContainer} from './RecipeList.style';
 import {Box, Grid, Typography, Card,
   CardContent, CardMedia, Divider} from '@mui/material';
+import NoMealsIcon from '@mui/icons-material/NoMeals';
 
 const RecipeList = (props) => {
-  const featuredList = () => {
-    console.log(props);
-    return (
-      <>
-        {props.recipes.map((recipe) => {
-          return (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={recipe.title}>
-              <Card
-                sx={{...recipeCard}}>
-                <CardMedia
-                  component="img"
-                  height='200'
-                  image= {recipe.image}
-                  alt= {`picture of ${recipe.title}`}
-                />
-                <Divider/>
-                <CardContent >
-                  <Typography variant="h6" component="div"
-                    sx={{...recipeTitle}}>
-                    {recipe.title.length > 45 ?
+  const featuredList = (recipes) => {
+    if (recipes.length <= 0) {
+      return (
+        <Box sx={{...recipeContainer, width: '100%'}}>
+          <NoMealsIcon color='primary' fontSize='large'/>
+          <Typography variant='h4' sx={{pl: 2, pr: 2}}> No Recipes Found</Typography>
+          <NoMealsIcon color='secondary' fontSize='large'/>
+        </Box>
+      );
+    } else {
+      return (
+        <>
+          {recipes.map((recipe) => {
+            return (
+              <Grid item xs={12} sm={6} md={4} lg={3} key={recipe.title}>
+                <Card
+                  sx={{...recipeCard}}>
+                  <CardMedia
+                    component="img"
+                    height='200'
+                    image= {recipe.image}
+                    alt= {`picture of ${recipe.title}`}
+                  />
+                  <Divider/>
+                  <CardContent >
+                    <Typography variant="h6" component="div"
+                      sx={{...recipeTitle}}>
+                      {recipe.title.length > 45 ?
                      `${recipe.title.substring(0, 45)}...` :
                       recipe.title }
-                  </Typography>
+                    </Typography>
 
-                </CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
-      </>
-    );
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+        </>
+      );
+    }
   };
 
   return (
@@ -47,7 +57,7 @@ const RecipeList = (props) => {
           </Typography>
           <Divider />
         </Grid>
-        {featuredList()}
+        {featuredList(props.recipes)}
       </Grid>
 
     </Box>
