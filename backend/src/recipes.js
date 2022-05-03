10846fe03984af3e73f6d83c0b1edda0d2cc9155
tkhayden <tylerkh805@gmail.com>
2022-05-02 17:11:25 -0700

    added logic to return remaining recipe results to response object

--- backend/src/recipes.js
+++ backend/src/recipes.js
@@ -11,8 +11,12 @@ exports.searchRecipes = async (req, res) => {
     const searchVal = req.query.search;
     const offset = req.query.offset;
     const recipeResponse = await axios.get(`${spoonacular}?query=${searchVal}&offset=${offset}&number=20&apiKey=${apiKey}`);
-    console.log(recipeResponse.data.results);
-    res.status(200).json(recipeResponse.data.results);
+    const remainingResults = recipeResponse.data.totalResults - offset - 20;
+    const resObj = {
+      results: recipeResponse.data.results,
+      remaining: remainingResults > 0 ? remainingResults : 0,
+    };
+    res.status(200).json(resObj);
   } catch (error) {
     console.log(error);
     res.status(400).end();
@@ -27,7 +31,6 @@ exports.getFeaturedRecipes = async (req, res) => {
       res.status(200).json(cachedRecipes);
     } else {
       const featureResponse = await axios.get(`https://api.spoonacular.com/recipes/random?number=20&apiKey=${apiKey}`);
-      console.log(featureResponse.data);
 
       const keys = ['id', 'title', 'calories', 'image'];
       const filteredRecipes = featureResponse.data.recipes.map((recipe) => {
@@ -37,7 +40,6 @@ exports.getFeaturedRecipes = async (req, res) => {
         }
         return obj;
       });
-      console.log(filteredRecipes);
       myCache.set('featured_recipes', filteredRecipes, 3600);
       res.status(200).json(filteredRecipes);
     }
