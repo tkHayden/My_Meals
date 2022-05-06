769f554be468c8439f9c71787c93b0afe8b04950
tkhayden <tylerkh805@gmail.com>
2022-05-05 19:01:06 -0700

    functions to get and formate recipe details

--- backend/src/recipes.js
+++ backend/src/recipes.js
@@ -48,3 +48,58 @@ exports.getFeaturedRecipes = async (req, res) => {
     res.status(400).end();
   }
 };
+
+exports.getRecipe = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const recipeResult = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${apiKey}`);
+    if (recipeResult.status == 200) {
+      const recipe = filterRecipe(recipeResult.data);
+      res.status(200).json(recipe);
+    } else {
+      res.status(404).end();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).end();
+  }
+};
+
+const filterRecipe = (recipe) => {
+  const keys = ['id', 'title', 'readyInMinutes', 'servings', 'image'];
+  const newRecipeObj = {instructions: []};
+  for (const key of keys) {
+    newRecipeObj[key] = recipe[key];
+  }
+  // Changing cuisines/diets from an array to string of concanated values to make it easier to display on the frontend
+  newRecipeObj.cuisines = recipe.cuisines.join(' , ');
+  newRecipeObj.diets = recipe.diets.join(' , ');
+  if (recipe.analyzedInstructions.length > 0) {
+    for (const instruction of recipe.analyzedInstructions[0].steps) {
+      newRecipeObj.instructions.push(instruction.step);
+    }
+  }
+  newRecipeObj.ingredients = grabMoreInfo(recipe.extendedIngredients, true);
+  newRecipeObj.nutrients = grabMoreInfo(recipe.nutrition.nutrients, false);
+
+  return newRecipeObj;
+};
+
+// Extracts appropriate properties from Ingredients or Nutrients obj from spoonacular's recipe obj response
+const grabMoreInfo = (obj, isIngredients) => {
+  const arr =[];
+  for (const item of obj) {
+    const tempObj = {};
+    tempObj['name'] = item.name;
+    tempObj['amount'] = Math.ceil(item.amount);
+    tempObj['unit'] = item.unit;
+    // Grab meta property only if we passed in the ingredient obj
+    if (isIngredients) {
+      // Changing meta from an array to string of concanated values to make it easier to display on the frontend
+      tempObj['meta'] = item.meta.join(' , ');
+    }
+    arr.push(tempObj);
+  }
+  return arr;
+};
