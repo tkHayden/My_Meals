c62cf5beac4d06c2148b225888e23bc542273cd7
tkhayden <tylerkh805@gmail.com>
2022-05-06 18:41:26 -0700

    styled components for Recipe.js

---
+++ frontend/src/components/RecipeView/Recipe.style.js
@@ -0,0 +1,20 @@
+
+import {Grid, List} from '@mui/material';
+import {styled} from '@mui/material/styles';
+
+export const GridItem = styled(Grid)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+
+export const InfoList = styled(List)({
+  'width': '95%',
+  'maxWidth': 390,
+  'bgcolor': 'background.paper',
+  'position': 'relative',
+  'overflow': 'auto',
+  'maxHeight': 300,
+
+});
