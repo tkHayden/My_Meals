37b6e8a82cdb96469e5dacbbe4e569169eea38d1
tkhayden <tylerkh805@gmail.com>
2022-05-10 17:22:56 -0700

    added testing route for individual recipes. One route when there is a background content and one from a copied url

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, useLocation} from 'react-router-dom';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import Home from './Home';
 import Recipes from './RecipeView/Recipes';
 import FeaturedRecipes from './RecipeView/FeaturedRecipes';
 import ResultRecipes from './RecipeView/ResultRecipes';
+import Recipe from './RecipeView/Recipe';
+import RecipeModal from './RecipeView/RecipeModal';
+import Layout from './Layout';
 
 const xtheme = createTheme({
   palette: {
@@ -34,16 +37,28 @@ const xtheme = createTheme({
 });
 
 const App = () => {
+  const location = useLocation();
+  const state = location.state;
+
   return (
     <ThemeProvider theme={xtheme}>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="recipes" element={<Recipes />}>
-          <Route index element={<FeaturedRecipes/>}/>
-          <Route path='results' element={<ResultRecipes/>}/>
-        </Route>
+      <Layout>
+        <Routes location={state?.backgroundLocation || location}>
+          <Route path="/" element={<Home />}/>
+          <Route path="recipes" element={<Recipes />}>
+            <Route index element={<FeaturedRecipes/>}/>
+            <Route path='results' element={<ResultRecipes/>}/>
+          </Route>
+          <Route path='/recipe' element ={<Recipe/>} />
 
-      </Routes>
+        </Routes>
+      </Layout>
+      {/* Show the modal when a `backgroundLocation` is set */}
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route path="/recipe" element={<RecipeModal/>} />
+        </Routes>
+      )}
     </ThemeProvider>
 
   );
