6d65528068ee7ae9f02aa030b0a9b0eb4ca3392b
tkhayden <tylerkh805@gmail.com>
2022-05-10 17:23:18 -0700

    added route to home link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,7 +10,6 @@ const Footer = () => {
   return (
     <>
       <Box sx={{
-        mt: 8,
         boxShadow: '-1px -1px 10px',
         background: 'linear-gradient( #7AB2B9, #E28657)'}}>
         <Grid container spacing={1} >
