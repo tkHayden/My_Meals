f6766263af23f7f4cf1363390d226f5d2e3adbdf
tkhayden <tylerkh805@gmail.com>
2022-05-12 12:40:27 -0700

    grocery_list and grocery_recipe table added

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,6 +1,15 @@
-DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS grocery_recipe CASCADE;
+DROP TABLE IF EXISTS grocery_list CASCADE;
 
-CREATE TABLE member(
-  id VARCHAR(60) UNIQUE PRIMARY KEY NOT NULL,
-  username VARCHAR(60)
+CREATE TABLE grocery_list (
+  id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE grocery_recipe (
+  id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
+  recipe_id INTEGER NOT NULL,
+  quantity INTEGER DEFAULT 1,
+  grocery_list_id UUID NOT NULL,
+  FOREIGN KEY (grocery_list_id) REFERENCES grocery_list(id) ON DELETE CASCADE
 )
