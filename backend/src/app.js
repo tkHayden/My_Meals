600b083f7da5182199c8d963e82f665c7cb3e444
tkhayden <tylerkh805@gmail.com>
2022-05-13 16:58:54 -0700

    moved checkJWT function to middleware.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,19 +8,13 @@ const path = require('path');
 const OpenApiValidator = require('express-openapi-validator');
 const recipes = require('./recipes');
 const user = require('./user');
-const {verifyUserId} = require('./util/middleware');
-
+const {verifyUserId, checkJwt} = require('./util/middleware');
 
 const app = express();
-const {auth} = require('express-oauth2-jwt-bearer');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-const checkJwt = auth({
-  audience: 'http://www.my-meals-api.com',
-  issuerBaseURL: `https://dev-ph7ct1gd.us.auth0.com/`,
-});
 
 app.get('/name', checkJwt, verifyUserId, user.getName);
 const apiSpec = path.join(__dirname, '../api/openapi.yaml');
