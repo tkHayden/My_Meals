904a24e36982e2e96d15080080cf9b0ffb952ca9
tkhayden <tylerkh805@gmail.com>
2022-05-16 16:00:37 -0700

    functions for update and delete queries on grocery list table

--- backend/src/db.js
+++ backend/src/db.js
@@ -20,17 +20,50 @@ exports.getAllGroceryLists = async (userId) => {
 
 exports.createGroceryList = async (userId, listName) => {
   try {
-    const select = 'INSERT INTO grocery_list(list_name, user_id)' +
+    const insert = 'INSERT INTO grocery_list(list_name, user_id)' +
    ' VALUES ($1, $2) RETURNING id, list_name, user_id';
 
     const query = {
-      text: select,
+      text: insert,
       values: [listName, userId],
     };
     const {rows} = await pool.query(query);
-    return rows.length == 0 ? false : rows;
+    return rows.length == 0 ? null : rows;
   } catch (error) {
     console.log(error);
+    return null;
+  }
+};
+
+exports.deleteGroceryList = async (userId, listId) => {
+  try {
+    const del = 'DELETE FROM grocery_list WHERE id = $1 and user_id = $2' +
+     ' RETURNING list_name';
+
+    const query = {
+      text: del,
+      values: [listId, userId],
+    };
+    const {rows} = await pool.query(query);
+    return rows.length == 0 ? null : rows;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+exports.updateGroceryListName = async (userId, listId, newName) => {
+  try {
+    const update = 'UPDATE grocery_list SET list_name = $1' +
+     ' WHERE id = $2 and user_id = $3 RETURNING *';
+
+    const query = {
+      text: update,
+      values: [newName, listId, userId],
+    };
+    const {rows} = await pool.query(query);
+    return rows.length == 0 ? null : rows;
+  } catch (error) {
     return null;
   }
 };
