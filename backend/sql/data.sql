21a1ace8e0f953f725888d292dfa8d3376be45b2
tkhayden <tylerkh805@gmail.com>
2022-05-16 15:58:39 -0700

    test data for local development

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -1,7 +1,7 @@
 --- test data
 
-insert into grocery_list (id, user_id) values ('1f1afa4c-d22d-11ec-9d64-0242ac120002', 1);
-insert into grocery_list (id, user_id) values ('1f1afccc-d22d-11ec-9d64-0242ac120002', 2);
-insert into grocery_list (id, user_id) values ('1f1afe0c-d22d-11ec-9d64-0242ac120002', 1);
-insert into grocery_list (id, user_id) values ('1f1b00be-d22d-11ec-9d64-0242ac120002', 1);
-insert into grocery_list (id, user_id) values ('1f1b01e0-d22d-11ec-9d64-0242ac120002', 3);
+insert into grocery_list (id, list_name, user_id) values ('1f1afa4c-d22d-11ec-9d64-0242ac120002', 'list1', 1);
+insert into grocery_list (id, list_name, user_id) values ('1f1afccc-d22d-11ec-9d64-0242ac120002', 'list2', 2);
+insert into grocery_list (id, list_name, user_id) values ('1f1afe0c-d22d-11ec-9d64-0242ac120002', 'list3', 1);
+insert into grocery_list (id, list_name, user_id) values ('1f1b00be-d22d-11ec-9d64-0242ac120002', 'list4', 1);
+insert into grocery_list (id, list_name, user_id) values ('1f1b01e0-d22d-11ec-9d64-0242ac120002', 'list2', 3);
