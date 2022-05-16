3cd201239574a9b04fd89379e9b5441e32397527
tkhayden <tylerkh805@gmail.com>
2022-05-16 15:58:55 -0700

    schema update

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,7 +3,9 @@ DROP TABLE IF EXISTS grocery_list CASCADE;
 
 CREATE TABLE grocery_list (
   id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id VARCHAR(60) NOT NULL
+  list_name VARCHAR(60) NOT NULL,
+  user_id VARCHAR(60) NOT NULL,
+  UNIQUE (list_name, user_id)
 );
 
 CREATE TABLE grocery_recipe (
