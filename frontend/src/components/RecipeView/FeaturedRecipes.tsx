4b62e088f24aaea332164997f193d2baf6bcd33f
tkhayden <tylerkh805@gmail.com>
2022-06-03 12:53:56 -0700

    Added Error message when site cannot connect to server

--- frontend/src/components/RecipeView/FeaturedRecipes.tsx
+++ frontend/src/components/RecipeView/FeaturedRecipes.tsx
@@ -2,23 +2,40 @@ import React, {useEffect, useState} from 'react';
 import RecipeList from './RecipeList';
 import CircularProgress from '@mui/material/CircularProgress';
 import {BasicRecipe} from './Recipe.model';
+import ErrorMessage from '../ErrorMessage';
 
 const FeaturedRecipes = () => {
-  // eslint-disable-next-line max-len
+  const [isError, setIsError] = useState(false);
   const [featureRecipes, setFeatureRecipes] = useState<
     BasicRecipe[] | undefined
   >(undefined);
   useEffect(() => {
     fetch('http://localhost:3010/v0/featured_recipes')
-        .then((response) => response.json())
-        .then((data: BasicRecipe[]) => setFeatureRecipes(data));
+        .then((response) => {
+          return response.json();
+        })
+        .then((data: BasicRecipe[]) => setFeatureRecipes(data))
+        .catch((err) => {
+          console.log(err);
+          setIsError(true);
+        });
   }, []);
+
+  const displayLoading = (isError: boolean) => {
+    return (
+      <>
+        {isError ?
+             <ErrorMessage/>:
+      <CircularProgress />}
+      </>
+    );
+  };
   return (
     <>
-      {featureRecipes ? (
+      {featureRecipes && !isError ? (
         <RecipeList recipeList={featureRecipes} header={'Featured'} />
       ) : (
-        <CircularProgress />
+        displayLoading(isError)
       )}
     </>
   );
